e6a9099b701dad22c1c9cd071b0d1f870b691722
nazzar28 <talantbekovnazar28@gmail.com>
2024-04-23 13:05:39 +0600

    updated application properties

---
+++ src/main/java/com/example/arab_recording/ArabRecordingApplication.java
@@ -0,0 +1,13 @@
+package com.example.arab_recording;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArabRecordingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArabRecordingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/arab_recording/ArabRecordingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arab_recording;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArabRecordingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
